d2aa7a9fcec0241eb2f0aa7b8d318044418c4a1c
Brad Akemi <mirado.akemi@gmail.com>
2025-04-04 22:26:22 +0300

    types

---
+++ types/world-explorer.ts
@@ -0,0 +1,11 @@
+export interface Country {
+  code: string
+  name: string
+  capital: string
+  continent: string
+  language: string
+  currency: string
+  population: number
+  path?: string // SVG path for the country shape
+}
+
